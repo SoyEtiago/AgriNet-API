b11316e42247f834cb88cd435306c488d3a569dc
Etienne Samboni <etienne.samboni@uao.edu.co>
2024-10-05 22:56:57 -0500

    Initialize the AgriNet Springboot Project

---
+++ src/main/java/com/backend/AgriNet/AgriNetApplication.java
@@ -0,0 +1,13 @@
+package com.backend.AgriNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/AgriNet/AgriNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.AgriNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
