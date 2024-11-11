9b6c5407f06ff181e45ffbe0a00223ee1e81c0da
Etienne Samboni <etienne.samboni@uao.edu.co>
2024-11-10 19:31:24 -0500

    Partial CRUD implementation for Users

--- src/main/java/com/backend/AgriNet/Controller/UsuariosController.java
+++ src/main/java/com/backend/AgriNet/Controller/UsuariosController.java
@@ -1,17 +1,22 @@
 package com.backend.AgriNet.Controller;
 
+import com.backend.AgriNet.DTO.ActualizarCredencialesDTO;
+import com.backend.AgriNet.DTO.ActualizarNombreDTO;
+import com.backend.AgriNet.DTO.ActualizarReseñaDTO;
 import com.backend.AgriNet.DTO.UsuariosDTO;
+import com.backend.AgriNet.Exception.RecursoNoEncontradoException;
 import com.backend.AgriNet.Model.*;
 import com.backend.AgriNet.Service.IUsuariosService;
+import org.apache.coyote.Response;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/Usuarios")
@@ -42,6 +47,64 @@ public class UsuariosController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al guardar el usuario en la base de datos.");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocurrió un error inesperado al intentar guardar el usuario.");
+        }
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<UsuariosModel>> listarUsuarios() {
+        List<UsuariosModel> listaUsuarios = usuarioService.listarUsuarios();
+        return ResponseEntity.status(200).body(listaUsuarios);
+    }
+
+    @GetMapping("/listar/{_id}")
+    public ResponseEntity<?> listarUsuario(@PathVariable ObjectId _id) {
+        try {
+            UsuariosModel usuario = usuarioService.buscarUsuarioPorId(_id);
+            return ResponseEntity.status(HttpStatus.OK).body(usuario);
+        } catch (RecursoNoEncontradoException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PatchMapping("/actualizar/nombre/{_id}")
+    public ResponseEntity<String> actualizarNombre(@PathVariable ObjectId _id, @RequestBody ActualizarNombreDTO nombreDTO) {
+        try {
+            String mensaje = usuarioService.actualizarNombre(_id, nombreDTO.getNombre());
+            return ResponseEntity.status(HttpStatus.OK).body(mensaje);
+        } catch (RecursoNoEncontradoException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PatchMapping("/actualizar/reseña/{_id}")
+    public ResponseEntity<String> actualizarReseña(@PathVariable ObjectId _id, @RequestBody ActualizarReseñaDTO reseñaDTO) {
+        try {
+            String mensaje = usuarioService.actualizarReseña(_id, reseñaDTO.getReseña());
+            return ResponseEntity.status(HttpStatus.OK).body(mensaje);
+        } catch (RecursoNoEncontradoException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PatchMapping("/actualizar/password/{_id}")
+    public ResponseEntity<String> actualizarPassword(@PathVariable ObjectId _id, @RequestBody String nuevoPassword) {
+        try {
+            String mensaje = usuarioService.actualizarPassword(_id, nuevoPassword);
+            return ResponseEntity.status(HttpStatus.OK).body(mensaje);
+        } catch (RecursoNoEncontradoException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/eliminar/{_id}")
+    public ResponseEntity<String> eliminarUsuario(@PathVariable ObjectId _id) {
+        try {
+            String resultado = usuarioService.eliminarUsuario(_id);
+            return ResponseEntity.status(HttpStatus.OK).body(resultado);
+        } catch (RecursoNoEncontradoException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (DataAccessException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al eliminar el usuario de la base de datos.");
         }
     }
 }
--- src/main/java/com/backend/AgriNet/Service/IUsuariosService.java
+++ src/main/java/com/backend/AgriNet/Service/IUsuariosService.java
@@ -9,5 +9,12 @@ public interface IUsuariosService {
     public String guardarUsuario(UsuariosModel usuario);
     public List<UsuariosModel> listarUsuarios();
     public UsuariosModel buscarUsuarioPorId(ObjectId id);
+
+    String actualizarNombre(ObjectId id, String nuevoNombre);
+
+    String actualizarReseña(ObjectId id, String nuevaReseña);
+
+    String actualizarPassword(ObjectId id, String nuevoPassword);
+
     public String eliminarUsuario(ObjectId id);
 }
